06f179f9315f63c610388cb7652781276da29a15
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-02-05 03:09:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.querySelector('#left')
+const rightBtn = document.querySelector('#right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener("click", () => {
+    activeSlide++
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+    setBgTobody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener("click", () => {
+    activeSlide--
+    if (activeSlide < 0) {
+        activeSlide =  slides.length - 1
+    }
+    setBgTobody()
+    setActiveSlide()
+})
+
+setBgTobody()
+
+function setBgTobody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove('active')
+    })
+
+    slides[activeSlide].classList.add('active')
+}
